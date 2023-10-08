b98e6a9808819cbeddd9330e559f8b0b8410faee
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2023-10-08 10:56:20 -0400

    import different pages/screens to navigate to. -- used createStackNavigtor to use for routing

--- CustomizableCarPlay/navigator.js
+++ CustomizableCarPlay/navigator.js
@@ -1,3 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavaigator } from "@react-navigation/stack";
+
+import Landing from "./pages/landing";
+import login from "./pages/login";
+import register from "./pages/register";
+import home from "./pages/home";
+
+const stack = createStackNavaigator();
