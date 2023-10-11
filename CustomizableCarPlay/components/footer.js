6126fcf29a4d11430e1e8147ea92baed6e4682b5
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2023-10-11 00:56:41 -0400

    Created basic footer bar -- No navigation
    used placeholder when icons are pressed, currently alerts and notifies page is under construction
    
    imported and added footer to home .js for testing

---
+++ CustomizableCarPlay/components/footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import { Alert } from "react-native";
+
+const FooterBar = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 10px;
+  background-color: #f8f8f8;
+`;
+
+const FooterItem = styled.TouchableOpacity`
+  align-items: center;
+`;
+
+export const Footer = () => {
+  const showPlaceholderAlert = () => {
+    Alert.alert("Under Construction", "This feature is not available yet.");
+  };
+
+  return (
+    <FooterBar>
+      <FooterItem onPress={showPlaceholderAlert}>
+        <Icon name="create-outline" size={30} color="#4F8EF7" />
+      </FooterItem>
+      <FooterItem onPress={showPlaceholderAlert}>
+        <Icon name="person-outline" size={30} color="#4F8EF7" />
+      </FooterItem>
+      <FooterItem onPress={showPlaceholderAlert}>
+        <Icon name="chatbubbles-outline" size={30} color="#4F8EF7" />
+      </FooterItem>
+    </FooterBar>
+  );
+};
--- CustomizableCarPlay/pages/home.js
+++ CustomizableCarPlay/pages/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Footer } from "../components/footer";
 
 function Home() {
   return (
     <View>
       <Text>Hello, welcome to Home!</Text>
+      <Footer />
     </View>
   );
 }
