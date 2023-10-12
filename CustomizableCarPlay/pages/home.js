d0f5b5435ce52e8c852f1a0c693a36d93380ff22
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2023-10-12 08:51:09 -0400

    updated home.js with styled-components
    -- added components to use for styling
    -- added button to "start driving" which only currently conole.log("button pressed"

--- CustomizableCarPlay/pages/home.js
+++ CustomizableCarPlay/pages/home.js
@@ -1,18 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native";
+import styled from "styled-components/native";
 import { Footer } from "../components/footer";
 
+const Container = styled.View`
+  flex: 1;
+  background-color: #3c3c3c;
+`;
+
+const CenteredView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WhiteText = styled.Text`
+  color: #ffffff;
+  font-size: 24px;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #b0cac7;
+  padding-vertical: 10px;
+  padding-horizontal: 20px;
+  border-radius: 5px;
+  margin-top: 20px;
+
+  /* iOS Shadow Props */
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.3;
+  shadow-radius: 5px;
+
+  /* Android Shadow */
+  elevation: 4;
+`;
+
+const ButtonText = styled.Text`
+  color: #000000i;
+  font-size: 18px;
+`;
+
 function Home() {
+  const onPressStartDriving = () => {
+    console.log("Start Driving Pressed!");
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: "#3C3C3C" }}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: "#FFFFFF", fontSize: 24 }}>
-          CustomizableCarPlay!
-        </Text>
-      </View>
+    <Container>
+      <CenteredView>
+        <WhiteText>CustomizableCarPlay!</WhiteText>
+        <Button onPress={onPressStartDriving}>
+          <ButtonText>Start Driving</ButtonText>
+        </Button>
+      </CenteredView>
 
       <Footer />
-    </View>
+    </Container>
   );
 }
 
