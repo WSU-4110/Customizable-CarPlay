2d2e19c24f3908e2dd58be68b1d9b7a46d7f853f
Moin Syed <moinn5810@gmail.com>
2023-10-12 01:43:06 -0400

    made a page for testing colors

---
+++ CustomizableCarPlay/components/customizedcolor.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import { StyledButton, StyledButtonText } from "../StyledComponents";
+
+
+
+const[bgColor, setByColor] = useState('#FFF');
+
+return (
+    <View style={{ flex: 1, backgroundColor: bgColor }}>
+     
+      <Button title="Red" onPress={() => setBgColor('red')} />
+      <Button title="Blue" onPress={() => setBgColor('blue')} />
+      <Button title="Green" onPress={() => setBgColor('green')} />
+      
+      <StatusBar style="auto" />
+    </View>
+  );
