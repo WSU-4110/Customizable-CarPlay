c36bbd971c79d671f2155330fca79b4767cd0313
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2023-10-18 17:48:48 -0400

    Moved styledComponents from home to separate file
    organized files for cleaner file structure

--- CustomizableCarPlay/pages/home.js
+++ CustomizableCarPlay/pages/home.js
@@ -1,44 +1,13 @@
 import React from "react";
-import styled from "styled-components/native";
 import { Footer } from "../components/footer";
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #3c3c3c;
-`;
-
-const CenteredView = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const WhiteText = styled.Text`
-  color: #ffffff;
-  font-size: 24px;
-`;
-
-const Button = styled.TouchableOpacity`
-  background-color: #b0cac7;
-  padding-vertical: 10px;
-  padding-horizontal: 20px;
-  border-radius: 5px;
-  margin-top: 20px;
-
-  /* iOS Shadow Props */
-  shadow-color: #000;
-  shadow-offset: 0px 2px;
-  shadow-opacity: 0.3;
-  shadow-radius: 5px;
-
-  /* Android Shadow */
-  elevation: 4;
-`;
-
-const ButtonText = styled.Text`
-  color: #000000;
-  font-size: 18px;
-`;
+import {
+  WhiteText,
+  CenteredView,
+  Button,
+  Container,
+  ButtonText,
+} from "../stylePages/homePage";
 
 function Home() {
   const onPressStartDriving = () => {
