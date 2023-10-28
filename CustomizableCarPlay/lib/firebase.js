6fdb6970705c407a556eed88058d964ba30d9ccf
mshieldswykes <144389371+mshieldswykes@users.noreply.github.com>
2023-10-28 00:14:27 -0400

    Create firebase.js
    created file to store firebase config keys more securely

---
+++ CustomizableCarPlay/lib/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0S35P0uuC8BX-WvZH_HJTXBYHfhBAhgM",
+  authDomain: "customizable-carplay-ef401.firebaseapp.com",
+  projectId: "customizable-carplay-ef401",
+  storageBucket: "customizable-carplay-ef401.appspot.com",
+  messagingSenderId: "17748607274",
+  appId: "1:17748607274:web:1c55289dd428b8ed069e53",
+};
+export default firebaseConfig;
