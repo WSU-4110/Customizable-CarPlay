6ca205add2e28bdcbe05e62a11a7cb1f60ac9f61
mshieldswykes <144389371+mshieldswykes@users.noreply.github.com>
2023-10-28 00:16:07 -0400

    Update App.js
    removed firebase keys- moved them to firebase.js in lib folder

--- CustomizableCarPlay/App.js
+++ CustomizableCarPlay/App.js
@@ -4,16 +4,6 @@ import AppNavigator from "./navigator";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC0S35P0uuC8BX-WvZH_HJTXBYHfhBAhgM",
-  authDomain: "customizable-carplay-ef401.firebaseapp.com",
-  projectId: "customizable-carplay-ef401",
-  storageBucket: "customizable-carplay-ef401.appspot.com",
-  messagingSenderId: "17748607274",
-  appId: "1:17748607274:web:1c55289dd428b8ed069e53",
-};
-
 firebase.initializeApp(firebaseConfig);
 
 export default function App() {
