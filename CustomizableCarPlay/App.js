03e2ed76f6f2817d7928fba3764ca0db549f7f60
mshieldswykes <mshieldswykes@gmail.com>
2023-10-31 18:03:54 -0400

    Update App.js
    Trying to address errors when expo is ran. Needed to import firebase configuration path

--- CustomizableCarPlay/App.js
+++ CustomizableCarPlay/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import{ NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
-import firebase from 'firebase';
+import firebase from "firebase";
 import AppNavigator from "./navigator";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import firebaseConfig from "./lib/firebase";
 
 firebase.initializeApp(firebaseConfig);
 
