4856a69403483e5eaec72ce990e33d410fd54485
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2023-11-01 00:58:24 -0400

    add styling to custom side bar
    - added animation to move in and out of the screen as button is pressed
    
    - turned all styles into styled components
    
    - added styling to text and added a horizontal line

--- CustomizableCarPlay/components/customSideBar.js
+++ CustomizableCarPlay/components/customSideBar.js
@@ -1,25 +1,69 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Animated, Text } from "react-native";
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components/native";
+
+const SidebarWrapper = styled(Animated.View)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 52px;
+  width: 250px;
+  background-color: black;
+  align-items: center;
+
+  shadow-color: gray;
+  shadow-offset: 5px -5px;
+  shadow-opacity: 0.8;
+  shadow-radius: 5px;
+
+  elevation: 10;
+`;
+
+const CustomizeText = styled.Text`
+  font-size: 24px;
+  color: white;
+  align-items: center;
+  padding-top: 20px;
+  font-style: italic;
+  font-family: System;
+`;
+
+const HorizontalLine = styled.View`
+  height: 2px;
+  background-color: white;
+  width: 60%;
+  margin-top: 10px;
+`;
 
 const Sidebar = ({ isVisible, onClose }) => {
-  if (!isVisible) return null;
+  // Initialize the animated value
+  // start off screen
+  const slideAnim = useRef(new Animated.Value(-250)).current;
+
+  useEffect(() => {
+    if (isVisible) {
+      // once button click or isVisible == true, start animation
+      Animated.timing(slideAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      // if !isVisible bring sidebar back
+      Animated.timing(slideAnim, {
+        toValue: -250,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [isVisible]);
 
   return (
-    <View style={styles.sidebar}>
-      <Text>Sidebar Text Test</Text>
-    </View>
+    <SidebarWrapper style={{ left: slideAnim }}>
+      <CustomizeText>Customize</CustomizeText>
+      <HorizontalLine />
+    </SidebarWrapper>
   );
 };
-
-const styles = StyleSheet.create({
-  sidebar: {
-    position: "absolute",
-    left: 0,
-    top: 0,
-    bottom: 52,
-    width: 250,
-    backgroundColor: "gray",
-  },
-});
 
 export default Sidebar;
