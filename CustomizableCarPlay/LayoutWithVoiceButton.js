27f0c47f9f558e597a8df5ef7c85847d2b26b78a
Delbert Li <lidelbert421@gmail.com>
2023-11-16 14:31:37 -0500

    Changed the red button to a free to use microphone icon to make it look better

--- CustomizableCarPlay/LayoutWithVoiceButton.js
+++ CustomizableCarPlay/LayoutWithVoiceButton.js
@@ -1,18 +1,20 @@
 // `LayoutWithVoiceButton.js`
 import React from 'react';
-import { TouchableOpacity, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, StyleSheet, Image } from 'react-native';
 
 const LayoutWithVoiceButton = ({ children }) => {
   const onPressButton = () => {
-    // You can add any action here that you want to perform when the button is pressed
-    console.log('Button pressed!');
+    console.log('Microphone button pressed!');
   };
 
   return (
     <View style={{ flex: 1 }}>
       {children}
       <TouchableOpacity style={styles.voiceButton} onPress={onPressButton}>
-        {/* Add an icon or text inside the button */}
+        <Image
+          source={require('./images/icons8-microphone-64.png')} 
+          style={styles.image}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -23,12 +25,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 20,
     bottom: 20,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  image: {
+    width: 64,  
+    height: 64, 
+    borderRadius: 32,
   },
 });
 
