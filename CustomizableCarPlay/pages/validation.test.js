b745f24ebc32a0a910097920c61e5db25162d4a7
mshieldswykes <mshieldswykes@gmail.com>
2023-11-17 23:12:38 -0500

    unit test part 3
    third method used for unit testing with jest

--- CustomizableCarPlay/pages/validation.test.js
+++ CustomizableCarPlay/pages/validation.test.js
@@ -4,11 +4,18 @@ describe('Phone Number Formatting', () => {
   test('Format valid phone number', () => {
     const phoneNumber = '1234567890';
     expect(validation.formatPhoneNumber(phoneNumber)).toBe('(123) 456-7890');
-  });*/
+  });
 
 describe('Email Validation', () => {
   test('Valid email format', () => {
     const validEmail = 'test@example.com';
     expect(validation.validateEmail(validEmail)).toBe(true);
   });
-})
+});*/
+
+describe('Password Validation', () => {
+  test('Invalid password format', () => {
+    const invalidPassword = 'weakpassword';
+    expect(validation.validatePassword(invalidPassword)).toBe(false);
+  });
+});
