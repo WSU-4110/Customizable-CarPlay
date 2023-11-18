c4becaf82fb16c05d04c1662f765c5ab790588ce
mshieldswykes <mshieldswykes@gmail.com>
2023-11-17 23:15:15 -0500

    unit test part 4
    fourth method used for unit testing with jest

--- CustomizableCarPlay/pages/validation.test.js
+++ CustomizableCarPlay/pages/validation.test.js
@@ -11,11 +11,18 @@ describe('Email Validation', () => {
     const validEmail = 'test@example.com';
     expect(validation.validateEmail(validEmail)).toBe(true);
   });
-});*/
+});
 
 describe('Password Validation', () => {
   test('Invalid password format', () => {
     const invalidPassword = 'weakpassword';
     expect(validation.validatePassword(invalidPassword)).toBe(false);
+  });
+});
+*/
+describe('Vehicle Year Validation', () => {
+  test('Valid vehicle year format', () => {
+    const validYear = '2023';
+    expect(validation.validateVehicleYear(validYear)).toBe(true);
   });
 });
