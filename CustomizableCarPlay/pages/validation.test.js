8014d9f7acaf5f526e292bf5db51022e5ad81a99
mshieldswykes <mshieldswykes@gmail.com>
2023-11-17 23:05:36 -0500

    unit test part 2
    second method used for unit testing with jest

--- CustomizableCarPlay/pages/validation.test.js
+++ CustomizableCarPlay/pages/validation.test.js
@@ -1,8 +1,14 @@
 const validation = require('./validation');
-
+/*
 describe('Phone Number Formatting', () => {
   test('Format valid phone number', () => {
     const phoneNumber = '1234567890';
     expect(validation.formatPhoneNumber(phoneNumber)).toBe('(123) 456-7890');
+  });*/
+
+describe('Email Validation', () => {
+  test('Valid email format', () => {
+    const validEmail = 'test@example.com';
+    expect(validation.validateEmail(validEmail)).toBe(true);
   });
 })
