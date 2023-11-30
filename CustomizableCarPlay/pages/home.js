75c66969e0b051ba6c27eb4ec84998e42a6bba5e
Aluckyasian <116318874+Aluckyasian@users.noreply.github.com>
2023-11-30 00:51:12 -0500

    Update home.js

--- CustomizableCarPlay/pages/home.js
+++ CustomizableCarPlay/pages/home.js
@@ -1,7 +1,13 @@
 import React, { useState, useContext } from "react";
 import { Footer } from "../components/footer";
 import DraggableIcon from "../components/draggableIcon";
-import { ImageBackground, TouchableOpacity, Text, View } from "react-native";
+import {
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  View,
+  Linking,
+} from "react-native";
 import {
   WhiteText,
   CenteredView,
@@ -18,6 +24,8 @@ import whatsAppIcon from "../images/WhatsAppIcon.png";
 import BackgroundImage from "../images/bghomepage.png";
 import SecondBackgroundImage from "../images/pacmanBG.png";
 import LayoutContext from "../components/LayoutContext";
+import DefaultBackgroundImage from "../images/default.png";
+import WayneStateImage from "../images/wsu.png";
 
 function Home() {
   const [sidebarVisible, setSidebarVisible] = useState(false);
@@ -27,9 +35,63 @@ function Home() {
     console.log("Start Driving Pressed!");
   };
 
+  const openSpotify = () => {
+    const url = "https://spotify.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open Spotify:", err);
+    });
+  };
+
+  const openWeather = () => {
+    const url = "https://www.weather.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open website:", err);
+    });
+  };
+
+  const openTikTok = () => {
+    const url = "https://www.tiktok.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open website:", err);
+    });
+  };
+  const openWhatsApp = () => {
+    const url = "https://www.whatsapp.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open website:", err);
+    });
+  };
+
+  const openMaps = () => {
+    const url = "https://maps.google.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open website:", err);
+    });
+  };
+
+  const Instagram = () => {
+    const url = "https://www.instagram.com";
+    Linking.openURL(url).catch((err) => {
+      console.error("Failed to open website:", err);
+    });
+  };
+
   const getBackgroundImage = () => {
-    const image =
-      layout === "layoutOne" ? BackgroundImage : SecondBackgroundImage;
+    let image;
+    switch (layout) {
+      case "Default":
+        image = DefaultBackgroundImage;
+        break;
+      case "layoutOne":
+        image = BackgroundImage;
+        break;
+      case "layoutTwo":
+        image = SecondBackgroundImage;
+        break;
+      case "layoutThree":
+        image = WayneStateImage;
+        break;
+    }
     console.log(`Current background image: ${image}`);
     return image;
   };
@@ -44,23 +106,32 @@ function Home() {
         <DraggableIcon
           source={spotifyIcon}
           initialPosition={{ x: -170, y: 80 }}
+          onPress={openSpotify}
         />
         <DraggableIcon
           source={instagramIcon}
           initialPosition={{ x: -170, y: 120 }}
+          onPress={Instagram}
         />
-        <DraggableIcon source={mapIcon} initialPosition={{ x: -170, y: 160 }} />
+        <DraggableIcon
+          source={mapIcon}
+          initialPosition={{ x: -170, y: 160 }}
+          onPress={openMaps}
+        />
         <DraggableIcon
           source={tikTokIcon}
           initialPosition={{ x: -170, y: 200 }}
+          onPress={openTikTok}
         />
         <DraggableIcon
           source={weatherIcon}
           initialPosition={{ x: -170, y: 240 }}
+          onPress={openWeather}
         />
         <DraggableIcon
           source={whatsAppIcon}
           initialPosition={{ x: -170, y: 280 }}
+          onPress={openWhatsApp}
         />
         <View
           style={{ flex: 1, justifyContent: "flex-end", marginBottom: 150 }}
