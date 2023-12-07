0da13d844a2b5861a834d68950b09596dbd3b1c6
mshieldswykes <mshieldswykes@gmail.com>
2023-12-07 17:49:20 -0500

    update registervehicle.js

--- CustomizableCarPlay/pages/registervehicle.js
+++ CustomizableCarPlay/pages/registervehicle.js
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fffff',
+    backgroundColor: '#ffffff',
     padding: 16,
   },
   scrollView:{
