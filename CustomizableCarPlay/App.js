2c145d44504747ed5692f4392541ee1003ed191f
Aluckyasian <116318874+Aluckyasian@users.noreply.github.com>
2023-12-06 22:02:22 -0500

    Update App.js
    Adding QuickAccessButton to be used in App.js

--- CustomizableCarPlay/App.js
+++ CustomizableCarPlay/App.js
@@ -1,24 +1,18 @@
-
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { StatusBar } from "expo-status-bar";
 import { LayoutProvider } from "./components/LayoutContext";
 import AppNavigator from "./navigator";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-//import { EventRegister } from "react-native-event-listeners";
-import themeContext  from "./components/themeContext";
+import QuickAcessButton from './QuickAcessButton';
+import themeContext from "./components/themeContext";
 import theme from "./components/theme";
-import { EventRegister }  from "react-native-event-listeners";
+import { EventRegister } from "react-native-event-listeners";
 import { NavigationContainer, DarkTheme, DefaultTheme } from "@react-navigation/native";
-//import { DefaultTheme } from "styled-components";
+
 
 
 export default function App() {
   const theme = useContext(themeContext);
   const [mode, setMode] = useState(false);
-
- 
-  
- 
 
   useEffect(() => {
     let eventListener = EventRegister.addEventListener(
@@ -38,7 +32,9 @@ export default function App() {
     <StatusBar style="auto" />
     <themeContext.Provider value={mode === true ? theme.dark : theme.light}>
       <NavigationContainer theme = {mode === true ? DarkTheme : DefaultTheme}>
+      <QuickAcessButton>
          <AppNavigator />
+      </QuickAcessButton>
    </NavigationContainer>
     </themeContext.Provider>
     </LayoutProvider>
